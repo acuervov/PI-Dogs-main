90b68460eea47a50050ae336703bd6341c698f49
acuervov <acuervov@unal.edu.co>
2021-11-11 13:38:54 -0500

    Proyect done

--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -6,8 +6,30 @@ const { Dog, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const dog = {
+  id: "481eeadb-1b13-41ab-a945-7d7fafcde4b2",
   name: 'Pug',
+  altura: "23",
+  weight:  "345",
 };
+
+const Dog1 = {
+  weight: { imperial: '6 - 13', metric: '3 - 6' },
+  height: { imperial: '9 - 11.5', metric: '23 - 29' },
+  id: 1,
+  name: 'Affenpinscher',
+  bred_for: 'Small rodent hunting, lapdog',
+  breed_group: 'Toy',
+  life_span: '10 - 12 years',
+  temperament: 'Stubborn, Curious, Playful, Adventurous, Active, Fun-loving',
+  origin: 'Germany, France',
+  reference_image_id: 'BJa4kxc4X',
+  image: {
+    id: 'BJa4kxc4X',
+    width: 1600,
+    height: 1199,
+    url: 'https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg'
+  }
+}
 
 describe('Videogame routes', () => {
   before(() => conn.authenticate()
@@ -16,9 +38,23 @@ describe('Videogame routes', () => {
   }));
   beforeEach(() => Dog.sync({ force: true })
     .then(() => Dog.create(dog)));
-  describe('GET /dogs', () => {
-    it('should get 200', () =>
-      agent.get('/dogs').expect(200)
+  // describe('GET /dogs', () => {
+  //   it('should get 200', () =>
+  //     agent.get('/dogs').expect(200)
+  //   );
+  // });
+  describe('GET home/dogs', () => {
+    it('should get 173 items', () =>
+      agent.get('/home/dogs')
+      .then(dogs=>  expect(dogs.body).to.have.length(173))
     );
+    it('home/dogs/name-pinsher to have length 4', () =>
+      agent.get('/home/dogs/name-pinscher')
+      .then(dogs=> expect(dogs.body).to.have.length(4))
+    );
+    // it('home/dogs/id-1 to have length 1', () =>
+    //   agent.get('/home/dogs/id-1')
+    //   .then(dogs=> expect(dogs.body).to.have.length(1))
+    // ); hace un timeout, buscar como ajustar el timeout
   });
 });
